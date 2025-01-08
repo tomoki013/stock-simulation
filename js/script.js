32fdac56357a6430f9ace15b19c761aa4606c8ed
tomoki013 <tomoki_ttttt@icloud.com>
2025-01-09 04:24:59 +0900

    利益計算を統一して分かりやすくしました

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 (() => {
     // DOM要素の取得
-    const container = document.getElementById('companies');
-    const startButton = document.getElementById('startButton');
-    const timeDisplay = document.getElementById('timeDisplay');
-    const dayDisplay = document.getElementById('dayDisplay');
-    const popupMessage = document.getElementById('popupMessage');
-    const popupModal = document.getElementById('popupModal');
-    const overlay = document.getElementById('overlay');
-    const closePopupButton = document.getElementById('closePopupButton');
-    const totalStockValueDisplay = document.getElementById('totalStockValue');
+    const $container = document.getElementById('companies');
+    const $startButton = document.getElementById('startButton');
+    const $timeDisplay = document.getElementById('timeDisplay');
+    const $dayDisplay = document.getElementById('dayDisplay');
+    const $popupMessage = document.getElementById('popupMessage');
+    const $popupModal = document.getElementById('popupModal');
+    const $overlay = document.getElementById('overlay');
+    const $closePopupButton = document.getElementById('closePopupButton');
+    const $totalStock = document.getElementById('totalStock');
+    const $totalProfit = document.getElementById('totalProfit');
+    const $totalStockValueDisplay = document.getElementById('totalStockValue');
+    const $totalWrapper = document.querySelector('.total_wrapper');
 
     // 変数の定義
     const intervalSeconds = 2000;
@@ -26,10 +29,10 @@
 
     // 会社情報
     const companies = [
-        { name: "Company A", stockPrice: 1000, previousPrice: 1000, shares: 0, buyPrice: 0, totalStockValueAtBuy: 0 },
-        { name: "Company B", stockPrice: 1500, previousPrice: 1500, shares: 0, buyPrice: 0, totalStockValueAtBuy: 0 },
-        { name: "Company C", stockPrice: 1800, previousPrice: 1800, shares: 0, buyPrice: 0, totalStockValueAtBuy: 0 },
-        { name: "Company D", stockPrice: 2000, previousPrice: 2000, shares: 0, buyPrice: 0, totalStockValueAtBuy: 0 },
+        { name: "Company A", stockPrice: 1000, shares: 0, buyPrice: 0, totalStockValueAtBuy: 0 },
+        { name: "Company B", stockPrice: 1500, shares: 0, buyPrice: 0, totalStockValueAtBuy: 0 },
+        { name: "Company C", stockPrice: 1800, shares: 0, buyPrice: 0, totalStockValueAtBuy: 0 },
+        { name: "Company D", stockPrice: 2000, shares: 0, buyPrice: 0, totalStockValueAtBuy: 0 },
     ];
 
     // ページに表示させる要素
@@ -48,8 +51,12 @@
                 <button class="buy_button" data-index="${index}">株を購入</button>
                 <button class="sell_button" data-index="${index}">株を売却</button>
             `;
-            container.appendChild(companyDiv);
+            $container.appendChild(companyDiv);
         });
+
+        const totalWrapperHeight = $totalWrapper.offsetHeight;
+        const bodyMarginBottom = totalWrapperHeight + 10;
+        document.body.style.marginBottom = `${bodyMarginBottom}px`;
 
         document.querySelectorAll('.buy_button').forEach(button => {
             button.addEventListener('click', (event) => {
@@ -69,11 +76,11 @@
 
     // スタートボタンが押された時の処理
     document.addEventListener('DOMContentLoaded', () => {
-        startButton.addEventListener('click', () => {
+        $startButton.addEventListener('click', () => {
             if (isFirstDay) {
                 isFirstDay = false;
                 startClock();
-                startButton.style.display = 'none';
+                $startButton.style.display = 'none';
             }
         });
     });
@@ -105,7 +112,10 @@
             clearInterval(clockInterval);
             isMarketOpen = false;
             stopStockPriceUpdates();
-            showPopup("場外時間です。場内時間に戻るまで株価は変動しません。");
+            showPopup(`
+                場外時間です。場内時間に戻るまで株価は変動しません。
+                場内時間: 09:00 ~ ${closeHour}:${closeMinute}
+                `);
             setTimeout(startNextDay, 5000);
         }
 
@@ -124,8 +134,8 @@
     // 時刻と日時の表示を更新する関数
     function updateClockDisplay() {
         const formattedTime = `${('0' + currentHour).slice(-2)}:${('0' + currentMinute).slice(-2)}`;
-        timeDisplay.innerText = formattedTime;
-        dayDisplay.innerText = `${currentDay}日目`;
+        $timeDisplay.innerText = formattedTime;
+        $dayDisplay.innerText = `${currentDay}日目`;
     }
 
     // 株価の更新をスタートさせる関数
@@ -148,76 +158,81 @@
     function updateStockPrice(company, index) {
         const change = Math.floor(Math.random() * 100 - 45);
         company.stockPrice = Math.max(1, company.stockPrice + change);
-        const oldPrice = company.stockPrice;
         document.getElementById(`stockPrice-${index}`).innerText = `${company.stockPrice.toLocaleString()}円`;
 
-        const priceChange = company.stockPrice - company.previousPrice;
-        document.getElementById(`stockChangeDaily-${index}`).innerText = `株価増減: ${priceChange >= 0 ? "+" : ""}${priceChange.toLocaleString()}円`;
-        company.previousPrice = oldPrice;
+        document.getElementById(`stockChangeDaily-${index}`).innerText = `株価増減: ${change >= 0 ? "+" : ""}${change.toLocaleString()}円`;
 
         updateTotalStockValue(company, index);
         updateTotalAssets();
     }
 
+    // 利益を計算する関数
+    function calculateProfit(company, amount) {
+        return amount * (company.stockPrice - company.buyPrice);
+    }
+
     // 株の購入処理
     function buyStock(index) {
+        stopClock();
+
         if (!isMarketOpen) {
-            stopClock();
             showPopup("場外時間中は株を購入できません。");
             return;
         }
 
         const company = companies[index];
         const amount = parseInt(document.getElementById(`shareAmount-${index}`).value);
-        const totalCost = amount * company.stockPrice;
 
         if (isNaN(amount) || amount <= 0) {
-            stopClock();
             showPopup("株数は0以上を入力して下さい。");
             return;
         }
 
+        const totalCost = amount * company.stockPrice;
         if (balance >= totalCost) {
             balance -= totalCost;
             company.shares += amount;
             company.buyPrice = company.stockPrice;
-            company.totalStockValueAtBuy = company.shares * company.stockPrice;
             updateDisplay(index);
-            stopClock();
-            showPopup(`${company.name} の株を ${amount} 株、1株 ${company.stockPrice.toLocaleString()} 円で購入しました。合計： ${totalCost.toLocaleString()} 円`);
+            showPopup(`
+                ${company.name}
+                購入株数: ${amount} 株
+                株価: ${company.stockPrice.toLocaleString()} 円
+                合計： ${totalCost.toLocaleString()} 円`);
         } else {
-            stopClock();
             showPopup("残高が足りません。");
         }
     }
 
     // 株の売却処理
     function sellStock(index) {
+        stopClock();
+
         if (!isMarketOpen) {
-            stopClock();
             showPopup("場外時間中は株を売却できません。");
             return;
         }
 
         const company = companies[index];
         const amount = parseInt(document.getElementById(`shareAmount-${index}`).value);
-        const totalRevenue = amount * company.stockPrice;
 
         if (isNaN(amount) || amount <= 0) {
-            stopClock();
             showPopup("株数は0以上を入力して下さい。");
             return;
         }
 
         if (company.shares >= amount) {
+            const totalRevenue = amount * company.stockPrice;
+            const profit = calculateProfit(company, amount);
             balance += totalRevenue;
             company.shares -= amount;
-            const profit = totalRevenue - (company.buyPrice * amount);
             updateDisplay(index);
-            stopClock();
-            showPopup(`${company.name} 売却株数： ${amount} 株 金額： ${totalRevenue.toLocaleString()} 円 利益： ${profit >= 0 ? "+" : ""}${profit.toLocaleString()} 円`);
+            showPopup(`
+                ${company.name}
+                売却株数： ${amount} 株
+                金額： ${totalRevenue.toLocaleString()} 円
+                利益： ${profit >= 0 ? "+" : ""}${profit.toLocaleString()} 円`);
         } else {
-            stopClock();
             showPopup("保有株数が足りません。");
         }
     }
@@ -234,31 +249,34 @@
     function updateTotalStockValue(company, index) {
         const totalValue = company.shares * company.stockPrice;
         document.getElementById(`totalStockValue-${index}`).innerText = `保有株の総額： ${totalValue.toLocaleString()} 円`;
-        const stockValueChange = company.shares > 0 ? totalValue - company.totalStockValueAtBuy : 0;
-        document.getElementById(`stockPriceChange-${index}`).innerText = `利益： ${stockValueChange.toLocaleString()} 円`;
+        const amount = parseInt(document.getElementById(`shareAmount-${index}`).value);
+        if (amount > 0) {
+            const stockValueChange = calculateProfit(company, amount)
+            document.getElementById(`stockPriceChange-${index}`).innerText = `利益： ${stockValueChange.toLocaleString()} 円`;
+        }
     }
 
     // 総資産の表示の更新
     function updateTotalAssets() {
         totalStockValue = companies.reduce((acc, company) => acc + company.shares * company.stockPrice, 0);
         const totalAssets = balance + totalStockValue;
-        totalStockValueDisplay.innerText = `総資産： ${totalAssets.toLocaleString()} 円`;
+        $totalStockValueDisplay.innerText = `総資産： ${totalAssets.toLocaleString()} 円`;
     }
 
     // ポップアップの表示
     function showPopup(message) {
-        popupMessage.innerText = message;
-        popupModal.style.display = 'block';
-        overlay.style.display = 'block';
+        $popupMessage.innerText = message;
+        $popupModal.style.display = 'block';
+        $overlay.style.display = 'block';
     }
 
     // ポップアップの非表示
     function closePopup() {
-        popupModal.style.display = 'none';
-        overlay.style.display = 'none';
+        $popupModal.style.display = 'none';
+        $overlay.style.display = 'none';
     }
 
-    closePopupButton.addEventListener('click', () => {
+    $closePopupButton.addEventListener('click', () => {
         closePopup();
         if (isMarketOpen) {
             startClock();
